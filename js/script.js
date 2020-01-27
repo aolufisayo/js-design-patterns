468f9aa1b81a99a333cf7493951c11457400de5c
Olufisayo Akinmoju <hfisk000@email.cpcc.edu>
2020-01-27 21:31:44 +0100

    namespace design pattern

---
+++ js/script.js
@@ -0,0 +1,53 @@
+var com = com || {};
+com.phissy = com.phissy || {};
+com.phissy.jsdp = com.phissy.jsdp || {}
+com.phissy.jsdp.sudoChat = {
+	leadself: 'Me: ',
+	leadcomputer: "PC: ",
+	aSaid: ["This is a Cyber Chat"],
+	msgYes: "Yes, that's a great idea.",
+	msgNo: "No, that must be a mistake.",
+	aSassyStuff: ["Like mold on books, grow myths on history.",
+		"She moved like a poem and smiled like a sphinx.",
+		"As long as we don’t die, this is gonna be one hell of a story.",
+		"She laughed, and the desert sang.",
+		"You’ve got about as much charm as a dead slug."],
+	talk: function talk(msg) {
+		this.echo(this.leadself + msg)
+	},
+	replyYesNo: function replyYesNo() {
+		var msg = Math.random() > .5 ? this.msgYes : this.msgNo;
+		this.echo(this.leadcomputer + msg)
+	},
+
+	saySassyStuff: function saySassyStuff() {
+		var msg = this.aSassyStuff[Math.floor(Math.random() * this.aSassyStuff.length)];
+		this.echo(this.leadcomputer + msg)
+	},
+	echo: function echo(msg) {
+		this.aSaid.push("<div>" + msg + "</div>");
+		var asaidLength = this.aSaid.length;
+		var start = Math.max(asaidLength - 6, 0);
+		out = "";
+		for (var i = start; i < asaidLength; i++) {
+			out += this.aSaid[i]
+		}
+		$(".advert").html(out)
+		$("#talk span").text(msg)
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
