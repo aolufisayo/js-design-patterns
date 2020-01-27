ee37d3e1e75946ea5aabf03e125b0ba28ef92a46
Olufisayo Akinmoju <hfisk000@email.cpcc.edu>
2020-01-27 23:02:11 +0100

    revealing-module-pattern

--- js/script.js
+++ js/script.js
@@ -1,40 +1,43 @@
 var sudoChat = (function () {
-	var leadself = 'Me: ';
-	var leadcomputer = "PC: ";
-	var aSaid = ["This is a Cyber Chat"];
-	var msgYes = "Yes, that's a great idea.";
-	var msgNo = "No, that must be a mistake.";
-	var aSassyStuff = ["Like mold on books, grow myths on history.",
+	var _leadself = 'Me: ';
+	var _leadcomputer = "PC: ";
+	var _aSaid = ["This is a Cyber Chat"];
+	var _msgYes = "Yes, that's a great idea.";
+	var _msgNo = "No, that must be a mistake.";
+	var _aSassyStuff = ["Like mold on books, grow myths on history.",
 		"She moved like a poem and smiled like a sphinx.",
 		"As long as we don’t die, this is gonna be one hell of a story.",
 		"She laughed, and the desert sang.",
 		"You’ve got about as much charm as a dead slug."];
 
-	var echo = function (msg) {
-		aSaid.push("<div>" + msg + "</div>");
-		var asaidLength = aSaid.length;
+	var _echo = function (msg) {
+		_aSaid.push("<div>" + msg + "</div>");
+		var asaidLength = _aSaid.length;
 		var start = Math.max(asaidLength - 6, 0);
 		out = "";
 		for (var i = start; i < asaidLength; i++) {
-			out += aSaid[i]
+			out += _aSaid[i]
 		}
 		$(".advert").html(out)
 		$("#talk span").text(msg)
-	}
+	};
+	var talk = function (msg) {
+		_echo(_leadself + msg)
+	};
+	var replyYesNo = function () {
+		var msg = Math.random() > .5 ? _msgYes : _msgNo;
+		_echo(_leadcomputer + msg)
+	};
+
+	var saySassyStuff = function () {
+		var msg = _aSassyStuff[Math.floor(Math.random() * _aSassyStuff.length)];
+		_echo(_leadcomputer + msg)
+	};
 
 	return {
-		talk: function (msg) {
-			echo(leadself + msg)
-		},
-		replyYesNo: function () {
-			var msg = Math.random() > .5 ? msgYes : msgNo;
-			echo(leadcomputer + msg)
-		},
-
-		saySassyStuff: function () {
-			var msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length)];
-			echo(leadcomputer + msg)
-		},
+		talk: talk,
+		replyYesNo: replyYesNo,
+		saySassyStuff: saySassyStuff
 	}
 
 })()
