0b05e980168d567cc5707c7bf998e793d0d9ecd9
Olufisayo Akinmoju <hfisk000@email.cpcc.edu>
2020-01-28 09:59:05 +0100

    factory-pattern

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 (function (win, $) {
+	var RedCircle = function () {
+		this.item = $('<div class="circle"></div>');
+	}
+	var BlueCircle = function () {
+		this.item = $('<div class="circle" style="background:blue"></div>');
+	}
+	var CircleFactory = function () {
+		this.create = function (color) {
+			if (color == "blue") {
+				return new BlueCircle();
+			} else {
+				return new RedCircle();
+			}
+		}
+	}
 	var CircleGeneratorSingleton = (function () {
 		var instance;
 		function init() {
 			var _aCircle = [];
 			var _stage = $('.advert')
+			var _circleFactory = new CircleFactory();
 
 			function _position(circle, top, left) {
 				circle.css('top', top);
 				circle.css('left', left);
 			};
-			function create(top, left) {
-				var circle = $('<div class="circle"></div>');
+			function create(top, left, color) {
+				var circle = _circleFactory.create(color).item
 				_position(circle, top, left)
 				return circle;
 			};
@@ -44,6 +60,13 @@
 			var circle = cg.create(e.pageY - 25, e.pageX - 25)
 			cg.add(circle);
 
+		})
+		$(document).keypress(function (e) {
+			if (e.key === 'a') {
+				var cg = CircleGeneratorSingleton.getInstance();
+				var circle = cg.create(Math.floor(Math.random() * 600), Math.floor(Math.random() * 600), "blue")
+				cg.add(circle);
+			}
 		})
 
 	})
